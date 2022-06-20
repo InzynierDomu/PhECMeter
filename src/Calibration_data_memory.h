d28a87c5bef2da882f55f7d5ea1b170404a982a8
Inżynier Domu <inzynier.domu@gmail.com>
2022-06-20 23:17:42 +0200

    remove arduino stl

--- src/Calibration_data_memory.h
+++ src/Calibration_data_memory.h
@@ -2,15 +2,14 @@
 
 #include "Linear_function.h"
 
-#include <ArduinoSTL.h>
 #include <EEPROM.h>
 
 class Calibration_data_memory
 {
   public:
   Calibration_data_memory();
-  void save_ph_calibration(std::pair<Point, Point> points);
-  void save_ec_calibration(std::pair<Point, Point> points);
-  std::pair<Point, Point> load_ph_calibration();
-  std::pair<Point, Point> load_ec_calibration();
+  void save_ph_calibration(Point points[2]);
+  void save_ec_calibration(Point points[2]);
+  void load_ph_calibration(Point points[2]);
+  void load_ec_calibration(Point points[2]);
 };
--- src/Linear_function.cpp
+++ src/Linear_function.cpp
@@ -2,9 +2,9 @@
 
 Linear_function::Linear_function() {}
 
-Linear_function::Linear_function(Point first, Point second)
+Linear_function::Linear_function(Point points[2])
 {
-  set_points(first, second);
+  set_points(points);
 }
 
 double Linear_function::find_y(int x)
@@ -12,14 +12,8 @@ double Linear_function::find_y(int x)
   return (a * x + b);
 }
 
-void Linear_function::set_points(Point first, Point second)
+void Linear_function::set_points(Point points[2])
 {
-  a = (second.y - first.y) / (double)(second.x - first.x);
-  b = first.y - (a * first.x);
-}
-
-void Linear_function::set_points(std::pair<Point, Point> points)
-{
-  a = (points.second.y - points.first.y) / (double)(points.second.x - points.first.x);
-  b = points.first.y - (a * points.first.x);
+  a = (points[1].y - points[0].y) / (double)(points[1].x - points[0].x);
+  b = points[0].y - (a * points[0].x);
 }
