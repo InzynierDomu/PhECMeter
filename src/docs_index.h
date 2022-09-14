9719b63214374d6da142ee34eec5f12b40d0b9b8
Inżynier Domu <inzynier.domu@gmail.com>
2022-09-14 17:02:12 +0200

    Update docs_index.h

--- src/docs_index.h
+++ src/docs_index.h
@@ -2,6 +2,6 @@
  *
  * \section Main state machine
  *
- * <img src="https://github.com/InzynierDomu/PhECMeter/blob/master/schems/state_chart/work_schem.svg">
+ * \image html https://github.com/InzynierDomu/PhECMeter/blob/master/schems/state_chart/work_schem.svg
  *
  */
