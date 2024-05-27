cb3b198fdc7ac55b57aac9b6e5f89b4080976299
Inżynier Domu <inzynier.domu@gmail.com>
2024-05-27 22:57:16 +0200

    add loging autodeploy script

--- autodeploy.bat
+++ autodeploy.bat
@@ -1,66 +1,101 @@
-
 @echo off
 
-:: Check if PlatformIO is installed
-pip show platformio > nul 2>&1
-if %errorlevel% neq 0 (
-echo PlatformIO is not installed. Proceeding with installation...
-pip install platformio
-if %errorlevel% neq 0 (
-echo Error during PlatformIO installation.
-exit /b %errorlevel%
-) else (
-echo PlatformIO has been successfully installed.
-)
-) else (
-echo PlatformIO is already installed.
-)
+set log_file=deploy.log
+(
+    set current_datetime=%date% %time%
+    echo Log started at: %current_datetime%
+) > "%log_file%" 2>&1
 
-:: Create a temporary file with the result of the pip show platformio command
-pip show platformio > pip_show_platformio_info.txt
+echo check Python 
+(
+    echo Checking if Python is installed...
+    python --version
+    if %errorlevel% neq 0 (
+        echo Python is not installed. Please install Python and try again.
+        echo Press Enter to exit.
+        pause >nul
+        exit /b %errorlevel%
+    ) else (
+        echo Python is already installed.
+    )
+) >> "%log_file%" 2>&1
 
-:: Set the default location of the text file
-set file_path="pip_show_platformio_info.txt"
+echo check Platform IO
+(
 
-:: Search the text file for the line containing "Location:"
-for /f "delims=: tokens=1,*" %%A in ('findstr /n "Location:" "%file_path%"') do (
-set "line_number=%%A"
-set "location=%%B"
-)
+    :: Check if PlatformIO is installed
+    echo Checking if PlatformIO is installed...
+    pip show platformio > pip_check.txt
+    findstr /C:"WARNING: Package(s) not found" pip_check.txt
+    if %errorlevel% equ 0 (
+        echo PlatformIO is not installed. Proceeding with installation...
+        pip install platformio
+        if %errorlevel% neq 0 (
+            echo Error during PlatformIO installation.
+            echo Press Enter to exit.
+            pause >nul
+            exit /b %errorlevel%
+        ) else (
+            echo PlatformIO has been successfully installed.
+        )
+    ) else (
+        echo PlatformIO is already installed.
+    )
+)>> "%log_file%" 2>&1
 
-:: Remove leading and trailing spaces from the location
-set "location=%location:~10%"
+echo locate Platform IO
+(
+    :: Create a temporary file with the result of the pip show platformio command
+    pip show platformio > pip_show_platformio_info.txt
 
-:: Output everything after "Location:"
-echo Everything after "Location:" in line %line_number%: %location%
+    :: Set the default location of the text file
+    set file_path="pip_show_platformio_info.txt"
 
-:: Remove "lib\site-packages" from the location and add "Scripts\platformio.exe"
-set "platformio_exe_path=%location:lib\site-packages=Scripts\platformio.exe%"
-echo %platformio_exe_path%
+    :: Search the text file for the line containing "Location:"
+    for /f "delims=: tokens=1,*" %%A in ('findstr /n "Location:" "%file_path%"') do (
+    set "line_number=%%A"
+    set "location=%%B"
+    )
 
-:: Check if the platformio.exe file exists
-if exist "%platformio_exe_path%" (
-echo Path to platformio.exe: %platformio_exe_path%
-) else (
-echo Unable to find platformio.exe file. Check the installation path.
-)
+    :: Remove leading and trailing spaces from the location
+    set "location=%location:~10%"
 
-:: Path to the Arduino project
-set project_path=.\
+    :: Output everything after "Location:"
+    echo Everything after "Location:" in line %line_number%: %location%
 
-:: Arduino board type (e.g., uno, nano, etc.)
-set board_type=nanoatmega328_oled
+    :: Remove "lib\site-packages" from the location and add "Scripts\platformio.exe"
+    set "platformio_exe_path=%location:lib\site-packages=Scripts\platformio.exe%"
+    echo %platformio_exe_path%
 
-:: Port to which Arduino is connected
-set port=arduino_port
+    :: Check if the platformio.exe file exists
+    if exist "%platformio_exe_path%" (
+    echo Path to platformio.exe: %platformio_exe_path%
+    ) else (
+    echo Unable to find platformio.exe file. Check the installation path.
+    )
 
-:: Calling PlatformIO CLI to upload the project
-%platformio_exe_path% run --target upload -d %project_path% -e %board_type%
+    echo Setting the path to the Arduino project...
+    set project_path=.\
 
-:: Check if the operation was successful
-if %errorlevel% neq 0 (
-echo Error uploading the project.
-exit /b %errorlevel%
-) else (
-echo The project has been successfully uploaded to Arduino.
-)
+    echo Setting the Arduino board type...
+    set board_type=nanoatmega328_oled
+
+    echo Setting the port to which Arduino is connected...
+    set port=arduino_port
+
+    echo Calling PlatformIO CLI to upload the project...
+    %platformio_exe_path% run --target upload -d %project_path% -e %board_type%
+
+    echo Checking if the operation was successful...
+    if %errorlevel% neq 0 (
+        echo Error uploading the project.
+    ) else (
+        echo The project has been successfully uploaded to Arduino.
+    )
+
+    echo Log saved to %log_file%.
+) >> "%log_file%" 2>&1
+
+echo Press Enter to exit.
+:: Wait for user to press Enter before exiting
+pause >nul
